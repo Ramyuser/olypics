092b2b65d714ae70f83d1be36608c1428b60ca95
Ramyuser <89673479+Ramyuser@users.noreply.github.com>
2021-12-25 22:22:10 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+canvas = document.getElementById("myCanvas");
+ctx = canvas.getContext("2d");
+color = "black ";
+ctx.beginPath();
+ctx.strokeStyle = "grey";
+ctx.lineWidth = 2;
+ctx.rect(150, 143, 430, 200);
+ctx.Stroke;
+
+ctx.beginPath();
+ctx.strokeStyle = "bule";
+ctx.lineWidth = 2;
+ctx.arc(250, 210, 40, 0, 2 * Math.PI);
+ctx.Stroke;
+
+ctx.beginPath();
+ctx.strokeStyle = "black";
+ctx.lineWidth = 2;
+ctx.arc(350, 210, 40, 0, 2 * Math.PI);
+ctx.Stroke;
